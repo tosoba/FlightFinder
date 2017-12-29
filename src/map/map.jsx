90300d324da5efae9167322f4322152ca442769f
therealmerengue@gmail.com <therealmerengue@gmail.com>
2017-12-29 18:47:09 +0100

    Added key

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -11,7 +11,7 @@ export default class MyFancyComponent extends React.PureComponent {
 
         this.MapComponent = compose(
             withProps({
-                googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+                googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBIBBIRZlWkExS0aO5oeRHKqFZ-XjAXLgs&v=3.exp&libraries=geometry,drawing,places",
                 loadingElement: <div style={{ height: `100%` }} />,
                 containerElement: <div style={{ height: `300px` }} />,
                 mapElement: <div style={{ height: `100%` }} />,
